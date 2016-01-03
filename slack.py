bf306f128971f5664c359bec1efb7aa59924d37b
jasoneka <jasoneka@users.noreply.github.com>
2016-01-03 23:08:20 +0100

    Delete slack.py

--- slack.py
+++
@@ -1,4 +0,0 @@
-__author__ = 'Andrzej'
-#token: xoxp-12561374081-15389395472-15609970528-f08bd42afa
-'xoxp-12561374081-15389395472-15609970528-f08bd42afa'
-#pisanie: https://slack.com/api/chat.postMessage?token=xoxp-12561374081-15389395472-15609970528-f08bd42afa&channel=C0CGHDG4A&text=test&username=ajasonek&pretty=1&as_user=true
